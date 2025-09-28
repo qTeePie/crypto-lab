709f17cf039d8fd884c5f5d49afe8af906c263dc
qTeePie <isabel@chaparian.no>
2025-09-28 12:34:39 +0200

    refactor of gcd utils

--- src/utils/gcd.rs
+++ src/utils/gcd.rs
@@ -1,8 +1,25 @@
-/// Returns the GCD of the two input numbers `a` and `b`.
-pub fn gcd(a: i32, b: i32) -> i32 {
+/*
+ Invariant: r_k = x_k * a + y_k * b
+ Where k is the current iteration and a, b are fixed
+*/
+
+/// Computes the extended GCD of two integers `a` and `b`.`
+///
+/// - `a` is the first input integer
+/// - `b` is the second input integer
+///
+/// Special cases:
+/// - If `b == 0`, returns `a.abs()`
+pub fn gcd(a: i64, b: i64) -> i64 {
+    // avoids dividing by 0
+    if b == 0 {
+        return a.abs(); // convention calls for positive GCD
+    }
+
     let mut x = a;
     let mut y = b;
 
+    // equal numbers => GCD = themselves
     if x == y {
         return x;
     }
@@ -12,21 +29,42 @@ pub fn gcd(a: i32, b: i32) -> i32 {
     }
 
     while y > 0 {
-        let temp = x;
+        let new = x;
         x = y;
-        y = temp % y;
+        y = new % y;
     }
 
     return x;
 }
 
-/// The extended GCD algorithm computes integers u and v such that au + bv = GCD
+/// Computes the extended GCD of two integers `a` and `b`.
+///
+/// Returns a triple `(g, u, v)` such that:
+///
+/// ```text
+/// g = gcd(a, b)
+/// g = a*u + b*v
+/// ```
+///
+/// - `a` is the first input integer
+/// - `b` is the second input integer
+/// - `g` is always nonnegative
+/// - `u` and `v` are integers that satisfy Bézout's identity
+///
+/// Special cases:
+/// - If `b == 0`, returns `(a.abs(), a.signum(), 0)`
 pub fn extended_gcd(a: i64, b: i64) -> (i64, i64, i64) {
+    // avoids dividing by 0
+    // returns `v` set to 0, absolute value of `a` as GCD, `u` is set to -+1 depending on the sign of `a`
+    if b == 0 {
+        return (a.abs(), a.signum(), 0);
+    }
+
     let mut r0 = a;
     let mut r1 = b;
 
     let mut x0 = 1; // coeff for a
-    let mut x1 = 0;
+    let mut x1 = 0; // forward coeff for a
 
     while r1 != 0 {
         let q = r0 / r1;
